c428a651c301019e7fe38e42f9beaf59977112b2
bagaskaramadhan <bagaskaramadhan@gmail.com>
2021-01-25 18:10:15 +0700

    learn koa

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 const Koa = require('koa')
+const KoaRouter = require('koa-router')
 
-const server = new Koa()
+const server = new Koa();
+const router = new KoaRouter();
 
-server.use(async ctx => (ctx.body = "HELLO"))
+router.get("/get", (ctx) => (ctx.body = "INI Router GET"));
+
+server.use(router.routes()).use(router.allowedMethods());
 
 server.listen(3000, () => {
-    console.log('SERVER STARTED')
-})
+    console.log("SERVER STARTED");
+});
